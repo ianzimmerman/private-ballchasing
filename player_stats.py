f481881662a17a94261ddb46202094b040e9351d
Ian Zimmerman <ianzimmerman@gmail.com>
2021-05-24 08:31:56 -0700

    reformat print screen

--- player_stats.py
+++ player_stats.py
@@ -19,15 +19,15 @@ if __name__ == "__main__":
             
         for l in leaderboard:
             print(f"{l.display_name.ljust(20)}", end=" ")
-            print(f"MMR: {str(round(ENV.expose(l.rating), 1)).rjust(4)}", end="   ")
-            print(f"GP: {str(l.games_played).rjust(3)}", end="   ")
+            print(f"MMR: {str(round(ENV.expose(l.rating), 1)).rjust(4)}", end="  |  ")
+            print(f"GP: {str(l.games_played).rjust(3)}", end="  |  ")
 
             wr = l.wins/l.games_played
-            print(f"WR: {str(round(100*(wr))).rjust(3)}%", end="   ")
+            print(f"WR: {str(round(100*(wr))).rjust(3)}%", end="  |  ")
 
-            print(f"eWR: {str(round(100*(l.expected_winrate))).rjust(3)}%", end="   ")
+            print(f"eWR: {str(round(100*(l.expected_winrate))).rjust(3)}%", end="  |  ")
 
             wrae = (wr - l.expected_winrate)/l.expected_winrate
             print(f"WRaE: {str(round(100*(wrae))).rjust(3)}%")
     else:
-        raise NotImplementedError(f'"{args.stat}" is an unknown stat')
+        raise NotImplementedError(f'"{args.stat}" is an unknown stat. Try "python player_stats.py rating"')
